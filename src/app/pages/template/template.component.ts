d0f2a7d6192ab3eeccd61aa4f205d945c97fa331
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-08-28 13:23:31 -0500

    Template: ngModel - ngSubmit - Local references to HTML elements

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styleUrls: ['./template.component.scss']
+  styleUrls: ['./template.component.scss'],
 })
 export class TemplateComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  save() {}
 }
